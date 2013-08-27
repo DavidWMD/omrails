aa4da27f2fa237b767e5c532658152a1664721c0
David Wong <davidcwongmd@gmail.com>
2013-08-26 21:10:23 -0500

    Initial Commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Omrails::Application.config.secret_token = 'da85fe3a989d062c22a89bc647ff314e6a1c9be4a7682a178accb69c3d6acc89474f657e3ec26d963ca0c68c980d2d3c7420ced369b297762fea5d39c8e4e26c'
+Omrails::Application.config.secret_token = '0dff2b8257c3eec1b6dbd5bc3b8607a36873a44adc7cf82c083e28227164a04fc9e60000118e22a9d4e94dbfb4532027d62e63fabd808fa59b818b9a532c90b9'
